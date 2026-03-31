a6c35769b2e61c0334ee7abd6ebc55fb3d307316
Vasiliy <maildiskmail@gmail.com>
2026-03-31 15:53:12 +0300

    Изменен код класса

--- src/main/java/ru/aston/strategy/SortByNumberGrup.java
+++ src/main/java/ru/aston/strategy/SortByNumberGrup.java
@@ -1,13 +1,21 @@
 package ru.aston.strategy;
 
+import ru.aston.entity;
 import java.util.Comparator;
 import java.util.List;
 
 public class SortByNumberGrup implements SortStrategy {
 
     @Override
-    public void sort(List<?> list) {
-        // TODO: реализация сортировки
+    public void sort(List<Student> students) {
+        if (students == null || students.size() <= 1) {
+            return;
+        }
+        Comparator<Student> comparator = Comparator
+                .comparing(Student::getGroupNumber)
+                .thenComparingDouble(Student::getAverageScore)
+                .thenComparing(Student::getGradeBookNumber);
+
+        quickSort(students, comparator, 0, students.size() - 1);
     }
-    Comparator<> comparator = Comparator
 }
--- src/main/java/ru/aston/strategy/SortStrategy.java
+++ src/main/java/ru/aston/strategy/SortStrategy.java
@@ -1,5 +1,39 @@
 package ru.aston.strategy;
 
+import java.util.Comparator;
+import java.util.List;
+
 public interface SortStrategy {
-    void sort(List<> );
+    void sort(List<Student> students);
+
+    default void quickSort(List<Student> list, Comparator<Student> comparator, int left, int right) {
+        if (left >= right) {
+            return;
+        }
+        int pivotIndex = partition(list, comparator, left, right);
+        quickSort(list, comparator, left, pivotIndex - 1);
+        quickSort(list, comparator, pivotIndex + 1, right);
+    }
+
+    default int partition(List<Student> list, Comparator<Student> comparator, int left, int right) {
+        Student pivot = list.get(right);
+        int i = left - 1;
+
+        for (int j = left; j < right; j++) {
+            if (comparator.compare(list.get(j), pivot) <= 0) {
+                i++;
+                swap(list, i, j);
+            }
+        }
+        swap(list, i + 1, right);
+        return i + 1;
+    }
+
+    default void swap(List<Student> list, int i, int j) {
+        if (i != j) {
+            Student temp = list.get(i);
+            list.set(i, list.get(j));
+            list.set(j, temp);
+        }
+    }
 }

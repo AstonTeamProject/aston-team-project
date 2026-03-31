eba7687973079d771f4acb2158413df6cdb6f7d9
Vasiliy <maildiskmail@gmail.com>
2026-04-01 01:37:23 +0300

    Remove comments from Sorter

--- src/main/java/ru/aston/sorting/Sorter.java
+++ src/main/java/ru/aston/sorting/Sorter.java
@@ -4,10 +4,6 @@ import java.util.Comparator;
 import ru.aston.entity.CustomStudentCollection;
 import ru.aston.entity.Student;
 
-/**
- * Контекст для выбора стратегии сортировки.
- * Позволяет менять стратегию сортировки динамически.
- */
 public class Sorter {
 
     private SortStrategy sortStrategy;
@@ -16,19 +12,10 @@ public class Sorter {
         this.sortStrategy = sortStrategy;
     }
 
-    /**
-     * Установить стратегию сортировки.
-     * @param sortStrategy стратегия сортировки
-     */
     public void setStrategy(SortStrategy sortStrategy) {
         this.sortStrategy = sortStrategy;
     }
 
-    /**
-     * Выполнить сортировку коллекции.
-     * @param collection коллекция студентов
-     * @param comparator компаратор для сравнения студентов
-     */
     public void sort(CustomStudentCollection collection, Comparator<Student> comparator) {
         if (sortStrategy == null) {
             throw new IllegalStateException("Sort strategy is not set");
@@ -36,10 +23,6 @@ public class Sorter {
         sortStrategy.sort(collection, comparator);
     }
 
-    /**
-     * Получить текущую стратегию сортировки.
-     * @return текущая стратегия
-     */
     public SortStrategy getSortStrategy() {
         return sortStrategy;
     }

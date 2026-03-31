834aa08104662d22a2765fe3b03b40d61f2b247d
Vasiliy <maildiskmail@gmail.com>
2026-03-31 13:18:22 +0300

    Implement SortByNumberGrup class

---
+++ src/main/java/ru/aston/strategy/SortByNumberGrup.java
@@ -0,0 +1,11 @@
+package ru.aston.strategy;
+
+import java.util.List;
+
+public class SortByNumberGrup implements SortStrategy {
+
+    @Override
+    public void sort(List<?> list) {
+        // TODO: реализация сортировки
+    }
+}

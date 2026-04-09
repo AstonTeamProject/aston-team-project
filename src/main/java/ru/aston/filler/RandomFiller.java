ed6a1b5cb940d28851c09affbe72832b5d74de05
Viktor <maksimenko.viktor2003@mail.ru>
2026-04-09 15:58:35 +0300

    Rewritten RandomFiller

--- src/main/java/ru/aston/filler/RandomFiller.java
+++ src/main/java/ru/aston/filler/RandomFiller.java
@@ -2,58 +2,43 @@ package ru.aston.filler;
 
 import ru.aston.entity.Student;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
-
-import static java.util.Collections.shuffle;
 
 public class RandomFiller implements DataFiller {
     private final List<String> groupNames = List.of("МАШ", "ФИТ", "ТБ", "ИКБО", "ИППО");
 
     @Override
     public List<Student> fill(int size) {
-        return randomizeStudents(size);
+        return IntStream.range(0, size)
+                .mapToObj(i -> {
+                    return randomizeStudent();
+                })
+                .collect(Collectors.toList());
     }
 
-    public List<Student> randomizeStudents(int size) {
+    public Student randomizeStudent() {
         int maxYear = 26;
         int minYear = 21;
         int maxSubGroup = 4;
         int minSubGroup = 1;
-        
-        List<String> groupNumbers = new ArrayList<>();
-        for (int i = 1; i <= size; i++) {
-            StringBuilder groupNumber = new StringBuilder();
-            groupNumber.append((int) ((Math.random() * (maxYear - minYear)) + minYear));
-            groupNumber.append("-" + groupNames.get((int) ((Math.random() * (groupNames.size())))));
-            int subGroup = (int) (Math.random() * maxSubGroup) + minSubGroup;
-            groupNumber.append("-" + subGroup);
-            groupNumbers.add(groupNumber.toString());
 
-        }
+        StringBuilder groupNumber = new StringBuilder();
+        groupNumber.append((int) ((Math.random() * ((maxYear + 1) - minYear)) + minYear));
+        groupNumber.append("-" + groupNames.get((int) ((Math.random() * (groupNames.size())))));
+        int subGroup = (int) (Math.random() * maxSubGroup) + minSubGroup;
+        groupNumber.append("-" + subGroup);
 
-        List<Integer> gradeBookNumbers = new ArrayList<>();
-        for (int i = 1; i <= 999999; i++) {
-            gradeBookNumbers.add(i);
-        }
-        shuffle(gradeBookNumbers);
+        double averageScore = 10 * Math.random();
+        averageScore = Math.round(averageScore * 100.0) / 100.0;
 
-        List<Double> averageScores = new ArrayList<>();
-        for (int i = 1; i <= size; i++) {
+        int gradeBookNumber = (int) (Math.random() * 999999);
 
-            double randomValue = 10 * Math.random();
-            randomValue = Math.round(randomValue * 100.0) / 100.0;
-            averageScores.add(randomValue);
-        }
-
-        return IntStream.range(0, size)
-                .mapToObj(i -> Student.builder()
-                        .groupNumber(groupNumbers.get((int) (Math.random() * groupNumbers.size())))
-                        .averageScore(averageScores.get(i))
-                        .gradeBookNumber(gradeBookNumbers.get(i))
-                        .build())
-                .collect(Collectors.toList());
+        return Student.builder()
+                .groupNumber(groupNumber.toString())
+                .averageScore(averageScore)
+                .gradeBookNumber(gradeBookNumber)
+                .build();
     }
 }

9f125d826ed47a23d405ec8225aad2d9ab23d92b
Egor <makry32rus@yandex.ru>
2026-04-10 14:57:54 +0300

    конструктор и геттер

--- src/main/java/ru/aston/filler/FillerContext.java
+++ src/main/java/ru/aston/filler/FillerContext.java
@@ -7,11 +7,19 @@ import java.util.List;
 public class FillerContext {
     private DataFiller dataFiller;
 
+    public  FillerContext (DataFiller dataFiller){
+        this.dataFiller=dataFiller;
+    }
+
+    public DataFiller getDataFiller(){
+        return dataFiller;
+    }
+
     public void setDataFiller(DataFiller dataFiller) {
         this.dataFiller = dataFiller;
     }
 
-    public List<Student> fillStudents(int size) {
+    public List<Student> fill(int size) {
         if (dataFiller == null) {
             throw new IllegalStateException("DataFiller is not set.");
         }

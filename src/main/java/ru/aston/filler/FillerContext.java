7a6cc1c7e8875a1bc6835ebf615dcc1c6b9b5e05
Makry-cmd <Makry32rus@yandex.ru>
2026-04-10 20:17:03 +0300

    Feature/#17
    * FillerContext
    
    * Remove unwanted file
    
    * конструктор и геттер
    
    * конструктор и геттер V2

---
+++ src/main/java/ru/aston/filler/FillerContext.java
@@ -0,0 +1,28 @@
+package ru.aston.filler;
+
+import ru.aston.entity.Student;
+
+import java.util.List;
+
+public class FillerContext {
+    private DataFiller dataFiller;
+
+    public FillerContext(DataFiller dataFiller) {
+        this.dataFiller = dataFiller;
+    }
+
+    public DataFiller getDataFiller() {
+        return dataFiller;
+    }
+
+    public void setDataFiller(DataFiller dataFiller) {
+        this.dataFiller = dataFiller;
+    }
+
+    public List<Student> fill(int size) {
+        if (dataFiller == null) {
+            throw new IllegalStateException("DataFiller is not set.");
+        }
+        return dataFiller.fill(size);
+    }
+}

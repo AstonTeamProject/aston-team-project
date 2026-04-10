a52d24eb9bf56df3f876e715a21c06c2d1e8a564
InviZ17 <118619804+InviZ17@users.noreply.github.com>
2026-04-10 20:30:35 +0300

    Feature/#10
    * Added ParitySort
    
    * Various fixes
    
    * Removed unused code

---
+++ src/main/java/ru/aston/sorting/ParitySort.java
@@ -0,0 +1,28 @@
+package ru.aston.sorting;
+
+import ru.aston.entity.Student;
+
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ParitySort extends QuickSort {
+    @Override
+    public void sort(List<Student> list, Comparator<Student> comparator) {
+        if (list == null || list.size() <= 1) {
+            return;
+        }
+        List<Student> evenValues;
+        evenValues = list.stream().filter(i -> i.getGradeBookNumber() % 2 == 0).collect(Collectors.toList());
+        if (evenValues.size() > 0) {
+            quickSort(evenValues, 0, evenValues.size() - 1, comparator);
+            Iterator<Student> evenIterator = evenValues.iterator();
+            for (int i = 0; i < list.size() && evenIterator.hasNext(); i++) {
+                if (list.get(i).getGradeBookNumber() % 2 == 0) {
+                    list.set(i, evenIterator.next());
+                }
+            }
+        }
+    }
+}
